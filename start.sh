f4dd5cf100a38a0d7539b246c77567cd6aac7418
inomp3042 <inomp3042@naver.com>
2020-11-20 17:01:08 +0900

    로그 생성기, 분석 모듈 도커 환경 구성
    로그 생성기, 분석 모듈 Dockerfile 추가
    필요한 requirements 추가
    kafka, elastic 연결을 위한 코드 수정
    docker-compose 파일 추가
    start.sh 수정

--- start.sh
+++ start.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-start() {
+up() {
     echo "start log anticheat..."
     set -x
-    docker-compose -f kafka-docker-compose/docker-compose.yml -f elastic-kibana/docker-compose.yaml -f fluentd/docker-compose.yaml up -d --build
+    docker-compose -f kafka-docker-compose/docker-compose.yml \
+        -f elastic-kibana/docker-compose.yaml \
+        -f fluentd/docker-compose.yaml \
+        -f python3.7/docker-compose.yaml \
+        up -d --build
     set +x
 }
 
 down() {
     echo "down docker containers of log anticheat..."
     set -x
-    docker-compose -f kafka-docker-compose/docker-compose.yml -f elastic-kibana/docker-compose.yaml -f fluentd/docker-compose.yaml down --volume
+    docker-compose -f kafka-docker-compose/docker-compose.yml \
+        -f elastic-kibana/docker-compose.yaml \
+        -f fluentd/docker-compose.yaml \
+        -f python3.7/docker-compose.yaml \
+        down --volume
     set +x
 }
 
@@ -19,8 +27,8 @@ if [ $# -ne 1 ]; then
     exit 0
 fi
 
-if [ $1 == "start" ]; then
-    start
+if [ $1 == "up" ]; then
+    up
 elif [ $1 == "down" ]; then
     down
 fi
