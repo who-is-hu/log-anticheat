78f2600b07b09db598ef42bbafb473cfdb7c570e
who-is-hu <dydgndlaek97@gmail.com>
2020-11-22 03:22:30 -0800

    test clustering one new data

--- log-analysis-module/clustering.py
+++ log-analysis-module/clustering.py
@@ -5,31 +5,17 @@ import numpy as np
 class ClusteringMgr:
     kmeans = {}
     
-    # 처음 모델을 만들기 위함
     def __init__(self, X):
-        createClusteringModel(self,X)
+        self.createClusteringModel(X)
     
-    def preprocess(self, message):
-        data = json.loads(message)
-        kpd = data['kill'] / data['death']
-        data.pop('kill')
-        data.pop('death')
-        data.update({'kpd': kpd})
-        print(data)
-        return data.values()
-
     def createClusteringModel(self,X):
         print("create clustering model...")
         dataset = np.array(X)
         self.kmeans = KMeans(n_clusters=2, random_state=0).fit(dataset)
-        print("Done: clustering model created")
+        print("Done: clustering model created")   
 
     # 새로운 data의 소속 군집 반환
-    def predictNewData(data, append_to_dataset=False):
-        p_data = self.preprocess(data)
-        result = self.kmeans.predict(p_data)
-        #if append_to_dataset == True:
-        #    X = np.append(X, np.array([source['value']]), axis=0)
-        #    lables = np.append(lables, np.array(result))
+    def predictNewData(self, data, append_to_dataset=False):
+        result = self.kmeans.predict(data)
         return result
        
--- log-analysis-module/main.py
+++ log-analysis-module/main.py
@@ -7,29 +7,38 @@ import time
 
 # elasticsearch 서버 정보
 es = Elasticsearch("http://localhost:9200")
-# es.info()
 index = "test"
-
+#es.info()
 
 # kafka setup
-bootstrap_servers = ["localhost:9092"]
-topic_name = 'parsed-topic'
-group_name = 'group1'
-consumer = KafkaConsumer(topic_name, 
-                        bootstrap_servers=bootstrap_servers, 
-                        enable_auto_commit=False,
-                        group_id=group_name
-                        )
+# bootstrap_servers = ["localhost:9092"]
+# topic_name = 'parsed-topic'
+# group_name = 'group1'
+# consumer = KafkaConsumer(topic_name, 
+#                         bootstrap_servers=bootstrap_servers, 
+#                         enable_auto_commit=False,
+#                         group_id=group_name
+#                         )
 
 clusteringMgr = {}
 
+def preprocess(dict_obj):
+    #임시로 uid, rid, timestamp 제거
+    dict_obj.pop('user')
+    dict_obj.pop('rid')
+    dict_obj.pop('time')
+    return list(dict_obj.values())
+    
 def fetchAll(_index):
     res = es.search(index=_index, body={
         "query":{"match_all":{}}
     })
     docs = []
-    for dicto in res['hits']['hits']:
-        docs.append(dicto['_source'])
+    for record in res['hits']['hits']:
+        valueList = preprocess(record['_source'])
+        docs.append(valueList)
+    # for d in docs:
+    #     print(d)
     return docs
 
 def consume_loop():
@@ -39,7 +48,7 @@ def consume_loop():
             for msg in partition_batch:
                 message = msg.value.decode('utf-8')
                 try:
-                    dict_data = clusteringMgr.preprocess(message)
+                    dict_data = json.loads(message)
                     result = clusteringMgr.predictNewData(dict_data)
                     #if result == 0:
                     #    send to alertmodule
@@ -49,21 +58,23 @@ def consume_loop():
                     print(res)
                 except Exception as e:
                     print(e)
-
-        #if len(msg_pack) != 0:
-        #    print("========================")
-        #    docs = fetchAll('test')
-        #    print(docs)
-
         consumer.commit()
 
 if __name__ == '__main__':
-    # for i in range(100):
+    # insert data to ES
+    # for i in range(10):
     #     msg = '{"shot_acc":0.67,"headshot_rate":0.33,"kill":%d,"death":3,"assist":4,"max_kill_streak":7,"time":"2000-10-01 13:00:00","rid":"rid01","user":"uid01"}' % (i)
     #     res = es.index(index=index, doc_type="_doc", body=msg)
     #     print(res)    
     
+    # test clustering one new data
     clusteringMgr = ClusteringMgr(fetchAll(index))
-    #msg = '{"shot_acc":0.67,"headshot_rate":0.33,"kill":999,"death":3,"assist":4,"max_kill_streak":7,"time":"2000-10-01 13:00:00","rid":"rid01","user":"uid01"}'
+    print(clusteringMgr.kmeans.labels_)
+    msg = '{"shot_acc":0.67,"headshot_rate":0.33,"kill":999,"death":3,"assist":4,"max_kill_streak":7,"time":"2000-10-01 13:00:00","rid":"rid01","user":"uid01"}'
+    dicto = json.loads(msg)
+    p_data = [preprocess(dicto)] # convert 2d array
+    print(p_data)
+    result = clusteringMgr.predictNewData(p_data)
+    print('label of new data : %d' % (result))
 
-    consume_loop()
+    #consume_loop()
