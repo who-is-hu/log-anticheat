1ba13c5c526c3cc02b93fade524c2cef1aeee4b8
김용후 <dydgndlaek97@gmail.com>
2020-11-29 00:50:14 +0900

    feat: receive data from kafka, predict cluster, send elastsicsearch

--- log-analysis-module/main.py
+++ log-analysis-module/main.py
@@ -22,10 +22,11 @@ while elastic_connection_check == False:
     except Exception as e:
         print(e)
     time.sleep(3)
-es = Elasticsearch(es_server, timeout=30, max_retries=10, retry_on_timeout=True)
+es = Elasticsearch(es_server, timeout=30,
+                   max_retries=10, retry_on_timeout=True)
 print('elastic search connected')
 
-#kafka setup
+# kafka setup
 kafka_server = os.getenv('KAFKA_SERVER', "localhost:9092")
 bootstrap_servers = [kafka_server]
 topic_name = 'parsed-topic'
@@ -38,29 +39,32 @@ while kafka_connection_check == False:
     print('try connect kafka...')
     try:
         consumer = KafkaConsumer(topic_name,
-            bootstrap_servers=bootstrap_servers, 
-            enable_auto_commit=False,
-            group_id=group_name)
-        
+                                 bootstrap_servers=bootstrap_servers,
+                                 enable_auto_commit=False,
+                                 group_id=group_name)
+
         kafka_connection_check = True
     except Exception as e:
         print(e)
-    
+
     time.sleep(3)
 print('kafka connected')
 
 clusteringMgr = {}
 
+
 def preprocess(dict_obj):
-    #임시로 uid, rid, timestamp 제거
+    # 임시로 uid, rid, timestamp 제거
     dict_obj.pop('user')
     dict_obj.pop('rid')
     dict_obj.pop('time')
+    print(dict_obj)
     return list(dict_obj.values())
-    
+
+
 def fetchAll(_index):
     res = es.search(index=_index, body={
-        "query":{"match_all":{}}
+        "query": {"match_all": {}}
     })
     docs = []
     for record in res['hits']['hits']:
@@ -70,36 +74,44 @@ def fetchAll(_index):
     #     print(d)
     return docs
 
+
 def consume_loop():
     while True:
         try:
             msg_pack = consumer.poll(timeout_ms=500)
             for partition_batch in msg_pack.values():
                 for msg in partition_batch:
-                    message = msg.value.decode('utf-8')
+                    print("===================================")
+                    message = msg.value
                     print(message)
-                
                     dict_data = json.loads(message)
-                    result = clusteringMgr.predictNewData(preprocess(dict_data))
-                    #if result == 0:
+                    result = clusteringMgr.predictNewData(
+                        [preprocess(dict_data)])[0]
+                    # if result == 0:
                     #    send to alertmodule
-                    dict_data.update({'label':result})
+                    dict_data.update({'label': result})
+                    print('predict done')
+                    print(dict_data)
                     jsonformat = json.dumps(dict_data)
-                    res = es.index(index=index, doc_type="_doc", body=jsonformat)
+                    print(jsonformat)
+                    res = es.index(index=index, doc_type="_doc",
+                                   body=jsonformat)
                     print(res)
             consumer.commit()
             time.sleep(1)
         except Exception as e:
             print(e)
 
+
 if __name__ == '__main__':
     # insert data to ES
-    for i in range(10):
-        msg = '{"shot_acc":0.67,"headshot_rate":0.33,"kill":%d,"death":3,"assist":4,"max_kill_streak":7,"time":"2000-10-01 13:00:00","rid":"rid01","user":"uid01"}' % (i)
-        res = es.index(index=index, doc_type="_doc", body=msg)
-        print(res)    
-    
-    time.sleep(5)
+    # for i in range(10):
+    #     msg = '{"shot_acc":0.67,"headshot_rate":0.33,"kill":%d,"death":3,"assist":4,"max_kill_streak":7,"time":"2000-10-01 13:00:00","rid":"rid01","user":"uid01"}' % (
+    #         i)
+    #     res = es.index(index=index, doc_type="_doc", body=msg)
+    #     print(res)
+    # time.sleep(5)
+
     # # test clustering one new data
     clusteringMgr = ClusteringMgr(fetchAll(index))
     # print(clusteringMgr.kmeans.labels_)
