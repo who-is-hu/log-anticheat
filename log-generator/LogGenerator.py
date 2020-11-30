0b95b3278a47d15d823eb7bceb5948a70a3a676d
siwar1565 <siwar1565@naver.com>
2020-12-01 02:04:00 +0900

    비정상 유저 구분, 랜덤 아이디, 추가 전적로그 데이터

--- log-generator/LogGenerator.py
+++ log-generator/LogGenerator.py
@@ -2,6 +2,7 @@ from kafka import KafkaProducer
 from kafka.errors import KafkaError
 import time
 import random
+import string
 import json
 import os
 #'''
@@ -56,11 +57,13 @@ while(True) :
     print("Log Data 생성")
 
     # logData.txt 읽기
+    #print("#logData.txt 읽기")
     logfiledir = os.getenv("LOG_FILE_DIR", "log-generator")
     logDataFile = open(os.path.join(logfiledir, "logData.txt"), "r") # 읽기 모드로 파일을 읽음
     line = logDataFile.readlines() # 파일을 줄단위로 list로 저장
 
     # log Data 생성
+    #print("#logData 생성")
     size = len(line)
     logData = line[random.randrange(1, size)] # 저장된 log random 선택, 0번째 줄은 순서를 써둔것이라 무시
     logDataSplit = logData.split("|") # 읽은 log를 분리
@@ -78,27 +81,44 @@ while(True) :
     10. 시간 = time
     '''
     for i in range(0,10) :
+        # create random ID
+        random_letter = string.ascii_letters + string.digits
+        randomID = ""
+        for j in range(6) :
+            randomID += random.choice(random_letter)
+
         # log Data random create
-        logFormat['user']['uid'] = 'ID0' + str(i)
-        logFormat['user']['name'] = 'Name0' + str(i)
-        logFormat['user']['rank'] = int(logDataSplit[0])
+        logFormat['user']['uid'] = 'ID' + randomID
+        logFormat['user']['name'] = 'Name' + randomID
+        logFormat['user']['rank'] = 1
         logFormat['round']['rid'] = logDataSplit[1]
         logFormat['round']['r_starttime'] = logDataSplit[2]
         logFormat['round']['r_endtime'] = logDataSplit[3]
-        logFormat['headshot_rate'] = int(logDataSplit[5]) + random.randrange(-5,6)
-        logFormat['kill'] = int(logDataSplit[6]) + random.randrange(-2,3)
-        logFormat['death'] = int(logDataSplit[7]) + random.randrange(-2,6)
-        logFormat['shot_acc'] = round((logFormat['kill']*100) / (logFormat['kill'] + logFormat['death']), 2)
-        logFormat['assist'] = int(logDataSplit[8]) + random.randrange(-5,6)
-        logFormat['max_kill_streak'] = int(logDataSplit[9]) + random.randrange(-5,6)
+        logFormat['shot_acc'] = round(float(logDataSplit[4]) + (float(logDataSplit[4]) * random.uniform(-0.15, 0.1)), 2)
+        logFormat['headshot_rate'] = round(float(logDataSplit[5]) + (float(logDataSplit[5]) * random.uniform(-0.15, 0.1)), 2)
+        logFormat['kill'] = int(logDataSplit[6]) + round(int(logDataSplit[6]) * random.uniform(-0.25, 0.15))
+        logFormat['death'] = int(logDataSplit[7]) + round(int(logDataSplit[7]) * random.uniform(-0.25, 0.15))
+        logFormat['assist'] = int(logDataSplit[8]) + round(int(logDataSplit[8]) * random.uniform(-0.25, 0.15))
+        logFormat['max_kill_streak'] = int(logDataSplit[9]) + round(int(logDataSplit[9]) * random.uniform(-0.25, 0.15))
         logFormat['time'] = logDataSplit[10]
 
-        # random data range error handling
-        if (logFormat['headshot_rate'] < 0) : logFormat['headshot_rate'] = 0            
-        if (logFormat['kill'] < 0) : logFormat['kill'] = 0            
-        if (logFormat['death'] < 0) : logFormat['death'] = 0
-        if (logFormat['assist'] < 0) : logFormat['assist'] = 0
-        if (logFormat['max_kill_streak'] > logFormat['kill']) : logFormat['max_kill_streak'] = logFormat['kill']
+        # random data handling
+        # abnormal data
+        if logDataSplit[0] == str(i) : 
+            logFormat['shot_acc'] = round(random.uniform(0.8, 1.0), 2)
+            logFormat['headshot_rate'] = round(random.uniform(0.8, 1.0), 2)
+            logFormat['kill'] = round(int(logDataSplit[6]) * 1.5)
+            logFormat['death'] = round(int(logDataSplit[7]) / 2)
+            logFormat['max_kill_streak'] = round(logFormat['kill'] * random.uniform(0.8, 1.0))
+
+        # normal data
+        else :
+            if logFormat['shot_acc'] < 0.0 : logFormat['shot_acc'] = 0.0
+            if logFormat['headshot_rate'] < 0.0 : logFormat['headshot_rate'] = 0.0
+            if logFormat['kill'] < 0 : logFormat['kill'] = 0
+            if logFormat['death'] < 0 : logFormat['death'] = 0
+            if logFormat['assist'] < 0 : logFormat['assist'] = 0
+            if logFormat['max_kill_streak'] > logFormat['kill'] : logFormat['max_kill_streak'] = logFormat['kill']
         
         print(logFormat)
         print("")
